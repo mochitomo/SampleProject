df2787d82ca485b59385ea3c2b6d463e6b89ed1c
Mochi <t_mochizuki@tafs.co.jp>
2017-06-06 11:41:12 +0900

    package名を変更した。

--- Practice/src/Sample/Sample1.java
+++ Practice/src/Sample/Sample1.java
@@ -1,4 +1,4 @@
-package Sample;
+package sample;
 
 public class Sample1 {
 
---
+++ Practice/src/sample/test/Sample1Test.java
@@ -0,0 +1,12 @@
+package sample.test;
+
+import org.junit.Test;
+
+public class Sample1Test {
+
+	@Test
+	public void test() {
+		System.out.println("Testが実行されました");
+	}
+
+}
